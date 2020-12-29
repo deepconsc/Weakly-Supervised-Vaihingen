fc722e4907762acaf8c51aa38652082a2b2a9042
aleksi_kistauri <lchkhetiani@systemcorp.ai>
2020-12-29 13:42:28 +0400

    refactor, iou

--- utilities/patches.py
+++ utilities/patches.py
@@ -9,6 +9,7 @@ import cv2
 import torch 
 import numpy as np 
 from subprocess import run 
+from dataset.patch_recover import patch as cutout
 
 midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small").cuda().eval()
 midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
@@ -35,27 +36,48 @@ def process(img):
     
     
         
-def patch(img, mask, num_generated, count, train=True):
-    max_start_width = img.shape[1] - 100
-    max_start_height = img.shape[0] - 100
-    
-    for x in tqdm.tqdm(range(num_generated)):
-        center = (randint(100, max_start_height), randint(100, max_start_width))
-        temporary_img = process(img[center[0]-100:center[0]+100, center[1]-100:center[1]+100])
-        temporary_mask = mask[center[0]-100:center[0]+100, center[1]-100:center[1]+100]
+def patch(img, mask, num_generated, count, train=True, custom=False):
+
+    if train:
+        path = 'train'
+    else: 
+        path = 'val'
+
+    if not custom:
+        max_start_width = img.shape[1] - 100
+        max_start_height = img.shape[0] - 100
         
-        if train:
-            path = 'train'
-        else: 
-            path = 'val'
-        np.save(f'{path}/image_{x}_{count}', temporary_img )
-        np.save(f'{path}/mask_{x}_{count}', temporary_mask )
+        for x in tqdm.tqdm(range(num_generated)):
+            center = (randint(100, max_start_height), randint(100, max_start_width))
+            temporary_img = process(img[center[0]-100:center[0]+100, center[1]-100:center[1]+100])
+            temporary_mask = mask[center[0]-100:center[0]+100, center[1]-100:center[1]+100]
+            
+            if train:
+                path = 'train'
+            else: 
+                path = 'val'
+            np.save(f'{path}/image_{x}_{count}', temporary_img)
+            np.save(f'{path}/mask_{x}_{count}', temporary_mask)
+
+    if custom: 
+        _, images = cutout(img)
+        _, masks = cutout(mask)
+        for x in tqdm.tqdm(range(len(images))):
+            temporary_img = process(images[x])
+            temporary_mask = masks[x]
+            np.save(f'validation/image_{x}_{count}', temporary_img)
+            np.save(f'validation/mask_{x}_{count}', temporary_mask)
+
+
         
         
-def generator(num_gt, num_total, train): 
-    mask_paths = glob.glob('gts_for_participants/*')[:num_gt]
+def generator(num_gt, num_total, train, custom): 
+    if custom:
+        mask_paths = glob.glob('gts_for_participants/*')[3:3+num_gt]
+    else:
+        mask_paths = glob.glob('gts_for_participants/*')[:num_gt]
     image_paths = [x.replace('gts_for_participants/', 'top/') for x in mask_paths]
     for a in range(len(image_paths)):
         image = cv2.cvtColor(cv2.imread(image_paths[a]), cv2.COLOR_BGR2RGB)
         mask = cv2.cvtColor(cv2.imread(mask_paths[a]), cv2.COLOR_BGR2RGB)
-        patch(image, mask, num_total, a, train)
+        patch(image, mask, num_total, a, train, custom)
